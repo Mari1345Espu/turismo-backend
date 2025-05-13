7f4ad5df7ff4a407354883c7274ac4d9f3db8b21
Mari1345Espu <mafeescobar3110@gmail.com>
2025-05-12 21:33:23 -0500

    Agrega vista home para ruta /

--- turismo_project/urls.py
+++ turismo_project/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home 
 
 
 urlpatterns = [
+     path('', home),
+     
     path('admin/', admin.site.urls),
     
     # Rutas de autenticación (Djoser + JWT)
