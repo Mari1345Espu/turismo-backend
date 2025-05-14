beee333e38d0631e08e2450cd33532890a5d2273
Mari1345Espu <mafeescobar3110@gmail.com>
2025-05-14 14:37:58 -0500

    Cambio db

--- turismo_project/wsgi.py
+++ turismo_project/wsgi.py
@@ -7,16 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turismo_project.settings')
 
 
-import django
-django.setup()
-from django.core.management import call_command
-call_command('migrate', interactive=False)
 
-application = get_wsgi_application()
